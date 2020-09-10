396af3300eab10c77504c6bd30266cf4617d35a9
Andrei-Catalin Monea <ac.monea99@gmail.com>
2020-09-10 15:06:36 +0300

    Flip JFK

---
+++ FlipDaCoins/FlipDaCoins/Coin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlipDaCoins
+{
+    class Coin
+    {
+
+        //constructor
+        public Coin(bool h)
+        {
+            isHeads = h;
+        }
+        //prop
+        public bool isHeads { get; set; }
+
+        //method
+        public void flipCoin( Random r)
+        {
+            if (r.Next(10) < 5)
+            {
+                isHeads = true;
+            }
+            else
+            {
+                isHeads = false;
+            }           
+        }
+    }
+}
---
+++ FlipDaCoins/FlipDaCoins/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlipDaCoins
+{
+    public partial class Form1 : Form
+    {
+        //array of coins
+        Coin[] coins = new Coin[5];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Random r = new Random();
+
+            label1.Text = "";
+
+            for (int i = 0; i < coins.Length ; i++)
+            {
+                //method for random fliping
+
+                coins[i].flipCoin(r);
+
+                //display the text 
+
+                if (coins[i].isHeads==true)
+                {
+                    label1.Text += "Heads" +",";
+                }
+                else
+                {
+                    label1.Text += "Tails" + ",";
+                }
+                
+            }
+
+            pictureBox1.Image = coins[0].isHeads ? Properties.Resources.coinHeads : Properties.Resources.coinTail;
+            pictureBox2.Image = coins[1].isHeads ? Properties.Resources.coinHeads : Properties.Resources.coinTail;
+            pictureBox3.Image = coins[2].isHeads ? Properties.Resources.coinHeads : Properties.Resources.coinTail;
+            pictureBox4.Image = coins[3].isHeads ? Properties.Resources.coinHeads : Properties.Resources.coinTail;
+            pictureBox5.Image = coins[4].isHeads ? Properties.Resources.coinHeads : Properties.Resources.coinTail;
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //loading the coins array 
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                coins[i] = new Coin(false);
+            }
+        }
+    }
+}
